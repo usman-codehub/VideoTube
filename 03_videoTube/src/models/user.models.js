2152e0736db9278904e5d4180752a0a148cc9479
Muhammad Usman <muhammadusman330539@gmail.com>
2026-07-01 12:01:00 +0500

    added schema for all models

---
+++ 03_videoTube/src/models/user.models.js
@@ -0,0 +1,54 @@
+import mongoose, { Schema } from "mongoose"
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: [true, "Username is Required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            required: [true, "Email is Required"],
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        fullname: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String,
+            required: true,
+        },
+        coverImage: {
+            type: String,
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            },
+        ],
+        password: {
+            type: String,
+            required: [true, "Password is Required"],
+
+        },
+        refreshToken: {
+            type: String
+        },
+
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const User = mongoose.model("User", userSchema) 
